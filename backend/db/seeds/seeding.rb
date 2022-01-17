b15d3a8c90871549256fe306a53642be948b77cb
Eti Noked <etinoked@Etis-MBP.lan>
2022-01-17 13:39:53 +0200

    changing items seeds cause wilco demand 100

--- backend/db/seeds/seeding.rb
+++ backend/db/seeds/seeding.rb
@@ -28,21 +28,34 @@ def seed_users
   end
 end
 
-def seed_items
-    csv_file_path = Rails.root.join('db', 'seeds', 'data', 'items.csv').to_s
-    puts "Seeding items from #{csv_file_path}..."
-    file = File.new(csv_file_path, 'r')
-    csv = CSV.new(file)
+# def seed_items
+#     csv_file_path = Rails.root.join('db', 'seeds', 'data', 'items.csv').to_s
+#     puts "Seeding items from #{csv_file_path}..."
+#     file = File.new(csv_file_path, 'r')
+#     csv = CSV.new(file)
   
-  csv.each do |row|
+#   csv.each do |row|
+#     item_information = {
+#       title: row[0],
+#       slug: row[1],
+#       description: row[2],
+#       image: row[3],
+#       user_id: row[4]
+#     }
+#     Item.create(item_information)
+#   end
+#   puts "Seeding items from #{csv_file_path} done."
+# end
+
+def seed_items
+  for i in 1..110 do
     item_information = {
-      title: row[0],
-      slug: row[1],
-      description: row[2],
-      image: row[3],
-      user_id: row[4]
+      title: "some title for item#{i}",
+      slug: "slug#{i}-#{rand()}",
+      description: "some description for item#{i}",
+      image: "image",
+      user_id: 50
     }
     Item.create(item_information)
   end
-  puts "Seeding items from #{csv_file_path} done."
 end
