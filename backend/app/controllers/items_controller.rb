cb7128899196dee3977986fd8aed33411b3ce521
Eti Noked <etinoked@Etis-MBP.lan>
2022-01-18 12:18:06 +0200

    try2

--- backend/app/controllers/items_controller.rb
+++ backend/app/controllers/items_controller.rb
@@ -2,7 +2,7 @@
 
 class ItemsSerializer < Jserializer::Base
   attributes :title, :slug, :description, :image, :created_at, :updated_at, :tag_list, :favorites_count
-
+  
   def image
     object.image || "placeholder.png"
   end
@@ -46,7 +46,8 @@ class ItemsController < ApplicationController
 
   def show
     @item = Item.find_by!(slug: params[:slug])
-    serializer = ItemsSerializer.new(@item)
+    #puts ItemsSerializer.new(@item)
+    serializer = ::ItemsSerializer.new(@item)
     render json: { item: serializer.serializable_hash }
   end
 
